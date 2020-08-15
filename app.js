06ca724c9b4f012e74ea4cdc30064b89e4f10dcd
João André Simioni <joao.simioni@horizonstelecom.com>
2020-08-14 23:00:01 -0300

    Automatic Commit

--- app.js
+++ app.js
@@ -25,5 +25,7 @@ app.use((req, res, next) => {
     res.status(404).render('404');
 });
 
-console.log("Listening port 3000");
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+console.log(`Listening port ${PORT}`);
+app.listen(PORT);
